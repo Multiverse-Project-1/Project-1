1896f767224bd0c6169fee6aa545f2cdf9ac7c57
coral Mejia <coral.mejia99@gmail.com>
2022-10-28 15:14:18 -0400

    updated spring security

--- src/main/java/com/Project1/models/User.java
+++ src/main/java/com/Project1/models/User.java
@@ -12,6 +12,9 @@ import javax.persistence.*;
     @Entity
     public class User {
         @Id @GeneratedValue (strategy = GenerationType.IDENTITY)
+        private int id;
+
+
         private String firstName;
 
 
